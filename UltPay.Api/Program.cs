116a04389f0dab541d68744d779381a0c4060523
BigBB23 <bigjava@gmail.com>
2026-04-04 21:04:29 -0400

    fixed program

--- UltPay.Api/Program.cs
+++ UltPay.Api/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using UltPay.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<UltPayDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
