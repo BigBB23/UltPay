0e27ef889b9818f621df7f9325ba56616b3ebe13
BigBB23 <bigjava@gmail.com>
2026-04-04 20:38:40 -0400

    p

--- UltPay.Api/Program.cs
+++ UltPay.Api/Program.cs
@@ -1,69 +1,8 @@
-using Microsoft.EntityFrameworkCore;
-using UltPay.Infrastructure.Persistence;
-using UltPay.Api.Services;
-using UltPay.Api.BackgroundServices;
-using UltPay.Api.Providers;
-
-using UltPay.Infrastructure.Providers.Flutterwave;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddScoped<ITransferExecutionService, TransferExecutionService>();
 builder.Services.AddControllers();
-builder.Services.AddScoped<IWalletService, WalletService>();
-builder.Services.AddDbContext<UltPayDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        sqlOptions =>
-        {
-            sqlOptions.EnableRetryOnFailure(
-                maxRetryCount: 5,
-                maxRetryDelay: TimeSpan.FromSeconds(10),
-                errorNumbersToAdd: null);
-        }));
-builder.Services.Configure<FlutterwaveOptions>(
-    builder.Configuration.GetSection("Flutterwave"));
-
-builder.Services.AddHttpClient<FlutterwaveTransferProvider, FlutterwaveTransferProvider>();
-
-
-builder.Services.AddScoped<ITransferProviderResolver, TransferProviderResolver>();
-
-//builder.Services.AddHostedService<TransferProcessorService>();
-
-builder.Services.AddScoped<ITransferExecutionService, TransferExecutionService>();
-
-builder.Services.AddScoped<ITransferProvider, FlutterwaveTransferProvider>();
-builder.Services.AddScoped<ITransferProviderResolver, TransferProviderResolver>();
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddDbContext<UltPayDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        sqlOptions =>
-        {
-            sqlOptions.EnableRetryOnFailure(
-                maxRetryCount: 5,
-                maxRetryDelay: TimeSpan.FromSeconds(10),
-                errorNumbersToAdd: null);
-        }));
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
