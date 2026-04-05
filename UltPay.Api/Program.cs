24c788297a4ef9f6c7d1f69765c9526c34282539
BigBB23 <bigjava@gmail.com>
2026-04-04 22:14:14 -0400

    program 3

--- UltPay.Api/Program.cs
+++ UltPay.Api/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using UltPay.Infrastructure.Persistence;
+using UltPay.Api.Services;
+using UltPay.Infrastructure.Providers.Flutterwave;
+using UltPay.Api.Providers;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +12,16 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<UltPayDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IWalletService, WalletService>();
+builder.Services.AddScoped<ITransferExecutionService, TransferExecutionService>();
+
+builder.Services.Configure<FlutterwaveOptions>(
+    builder.Configuration.GetSection("Flutterwave"));
+
+builder.Services.AddHttpClient<FlutterwaveTransferProvider>();
+
+builder.Services.AddScoped<ITransferProvider, FlutterwaveTransferProvider>();
+builder.Services.AddScoped<ITransferProviderResolver, TransferProviderResolver>();
 
 var app = builder.Build();
 
